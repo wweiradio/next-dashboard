96c71ec48b07e390cee27382d0aa1ea633878b97
Cason Wang <wweiradio@google.com>
2024-04-02 21:38:41 +0800

    second change

--- app/dashboard/page.tsx
+++
@@ -1,9 +0,0 @@
-import { noto } from '@/app/ui/fonts';
-
-export default function Page() {
-    return (
-        <div className={`${noto.className} text-red-900`} >
-            <p> Dashboard Page </p>
-        </div>
-    )
-}
